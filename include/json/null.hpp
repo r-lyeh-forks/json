a32d1350d04e46921123efd6b565a6e5a14d7baa
Tres Walsh <tres.walsh@mnmlstc.com>
2013-12-25 13:25:13 -0800

    Add json::null_t type

---
+++ include/json/null.hpp
@@ -0,0 +1,12 @@
+#ifndef JSON_NULL_HPP
+#define JSON_NULL_HPP
+
+namespace json {
+inline namespace v1 {
+
+struct null_t final { };
+constexpr null_t null { };
+
+}} /* namespace json::v1 */
+
+#endif /* JSON_NULL_HPP */
